837332e0943bc019199f46db0436bf20cbd8941a
Kaung Shein <nicholasling1412@gmail.com>
2022-06-06 02:43:30 -0700

    Weight styling

--- src/pages/EntryPage/Weight.jsx
+++ src/pages/EntryPage/Weight.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { query } from '../../util';
+import { getShortDateFromDate, query } from '../../util';
+import { HiCheck } from 'react-icons/hi';
+import { FiEdit2 } from 'react-icons/fi';
+import styles from './Weight.module.scss';
 
 const Weight = ({ currDate }) => {
   const [currentWeight, setCurrentWeight] = useState({ weight_id: '', weight_value: '' });
@@ -56,37 +59,44 @@ const Weight = ({ currDate }) => {
   };
 
   return (
-    <section>
-      <section>
-        <h3>Past Weights</h3>
-        <div>
-          {pastWeights.map((weight, idx) => {
-            return (
-              <div key={idx}>
-                <span>{weight.weight_date}</span>
-                {weight.weight_value}
-              </div>
-            );
-          })}
-        </div>
-      </section>
-      <section>
-        <h3>Current Weight</h3>
-        <form>
-          <input
-            type="number"
-            name="weight_value"
-            id="weight_value"
-            value={currentWeight.weight_value}
-            onChange={(event) => setCurrentWeight({ ...currentWeight, weight_value: event.target.value })}
-            disabled={!allowEdit}
-          />
-          {allowEdit ? (
-            <button onClick={handleSubmit}>Submit</button>
-          ) : (
-            <button onClick={handleEdit}>Edit</button>
-          )}
-        </form>
+    <section className={styles.container}>
+      <h2 className={styles.h2}>Weight</h2>
+      <section className={styles.inner_container}>
+        <section className={styles.past_container}>
+          <h3 className={styles.h3}>Past</h3>
+          <div className={styles.past_entries_container}>
+            {pastWeights.map((weight, idx) => {
+              return (
+                <p key={idx} className={styles.past_entry}>
+                  {getShortDateFromDate(weight.weight_date)}
+                  <span className={styles.weight}>{weight.weight_value} lb</span>
+                </p>
+              );
+            })}
+          </div>
+        </section>
+        <section className={styles.current_container}>
+          <h3 className={styles.h3}>Current</h3>
+          <form>
+            <input
+              type="number"
+              name="weight_value"
+              id="weight_value"
+              value={currentWeight.weight_value}
+              onChange={(event) => setCurrentWeight({ ...currentWeight, weight_value: event.target.value })}
+              disabled={!allowEdit}
+            />
+            {allowEdit ? (
+              <button onClick={handleSubmit} className={styles.submit_btn}>
+                <HiCheck size={20} className={styles.btn_logo} />
+              </button>
+            ) : (
+              <button onClick={handleEdit} className={styles.btn}>
+                <FiEdit2 size={15} className={styles.btn_logo} />
+              </button>
+            )}
+          </form>
+        </section>
       </section>
     </section>
   );
